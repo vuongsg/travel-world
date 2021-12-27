087dde8bd79858ab79e09cf687c3053a2267a101
vuongsg <vuong.goteborg@gmail.com>
2021-12-27 15:48:16 +0700

    indentation

--- src/components/CountryInfo.tsx
+++ src/components/CountryInfo.tsx
@@ -13,23 +13,17 @@ export const CountryInfo: React.FC<ICountryProps> = (props) => {
             </Grid>
             <Grid container direction='row'>
                 <Grid item sm={12} lg={3} style={{ textAlign: 'center', margin: '0 auto', marginBottom: 30, paddingLeft: 30 }}>
-                    <img src={country.flag} alt=''
-                        style={{ width: '100%', maxWidth: 250, maxHeight: 250, objectFit: 'cover' }} />
+                    <img src={country.flag} alt='' style={{ width: '100%', maxWidth: 250, maxHeight: 250, objectFit: 'cover' }} />
                 </Grid>
                 <Grid item sm={12} lg={9} id='grid-info' style={{ lineHeight: 2, margin: '0 auto', marginBottom: 30, paddingLeft: 30 }}>
                     <p>- Native name: {country.nativeName}</p>
-                    <p>- Official name: {country.altSpellings.length > 1
-                        ? country.altSpellings[1]
-                        : country.altSpellings.length > 0
-                            ? country.altSpellings[0]
-                            : ''}</p>
+                    <p>- Official name: {country.altSpellings.length > 1 ? country.altSpellings[1] 
+                                                                         : country.altSpellings.length > 0 ? country.altSpellings[0] : ''}</p>
                     <p>- Region: {country.subregion}</p>
                     <p>- Capital: {country.capital}</p>
                     <p>- {country.timezones.length > 1 ? 'Time zones' : 'Time zone'}:
                         <ul>
-                            {country.timezones.map(m => <li>
-                                {m.endsWith(':00')
-                                    ? m.substr(0, m.length - 3) : m}</li>)}
+                            {country.timezones.map(m => <li>{m.endsWith(':00') ? m.substr(0, m.length - 3) : m}</li>)}
                         </ul>
                     </p>
                     <p>- {country.callingCodes.length > 1 ? 'Codes' : 'Code'}:
@@ -51,14 +45,12 @@ export const CountryInfo: React.FC<ICountryProps> = (props) => {
                     <p>- {country.borders.length > 1 ? 'Borders' : 'Border'}:
                         <ul>
                             {country.borders
-                                .map(m => lstCountries
-                                    .find(co => co.alpha3Code === m))
-                                .map(m => <li>
-                                    <button className='btn-country-name'
-                                        onClick={event => onSelectCountryByName(event, m?.name)}>
-                                        {m?.name}
-                                    </button>
-                                </li>)}
+                                    .map(m => lstCountries.find(co => co.alpha3Code === m))
+                                    .map(m => <li>
+                                                <button className='btn-country-name' onClick={event => onSelectCountryByName(event, m?.name)}>
+                                                    {m?.name}
+                                                </button>
+                                            </li>)}
                         </ul>
                     </p>
                 </Grid>
